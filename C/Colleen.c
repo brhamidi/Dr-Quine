9dac40cd9dfe96cf3f95ccb7b1f7a7dd0bb2a82c
bhamidi <bhamidi@student.42.fr>
2018-07-31 18:56:11 +0200

    add Collen.c file

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,5 +1,3 @@
-/*
-  comment one
-*/
+#include <stdio.h>/*comment*/
 void ft(void){};
-int main(void){ft();/* comment two */}
+int main(void){ft();/*comment*/char *s="#include <stdio.h>/*comment*/%cvoid ft(void){};%cint main(void){ft();/*comment*/char *s=%c%s%c;printf(s,10,10,34,s,34,10);}%c";printf(s,10,10,34,s,34,10);}
