e42fb583a8fa7d8519afdd97137a99949efe66c9
bhamidi <bhamidi@student.42.fr>
2018-08-01 16:09:04 +0200

    wip

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -2,8 +2,14 @@
 #include <stdio.h>
 #include <fcntl.h>
 int i = 5;
-char n[] = "Sully_X.c";
-char const *a[]={""};
-void e(){}
-void f(){n[6]=48+i;char *s="";int fd=open(n,O_CREAT|O_RDWR,S_IRUSR|S_IRGRP|S_IROTH);e();close(fd);}
+char n[]="Sully_X.c";
+char prog[]="Sully_X";
+char const *a[]={"gcc","-Werror","-Wextra","-Wall","n","-o", prog};
+char const *b[]={prog, prog};
+void e(){
+	if (fork() == 0) execve(a[0],(char*const*)a,NULL);
+	else
+		if (fork() == 0) execve(b[0], (char*const*)b, NULL);
+}
+void f(){n[6]=48+i;prog[6]=48+i;/*char *s=""*/;int fd=open(n,O_CREAT|O_RDWR,S_IRUSR|S_IRGRP|S_IROTH);dprintf(fd,"int main(){return (0);}");e();close(fd);}
 int main(){if(i)f();}
