aea3c676950b77e057da737123a94e54328b3cc5
bhamidi <bhamidi@student.42.fr>
2018-08-01 18:36:15 +0200

    wip

--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -1,15 +1,13 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
 int i = 5;
 char n[]="Sully_X.c";
-char prog[]="Sully_X";
-char const *a[]={"gcc","-Werror","-Wextra","-Wall","n","-o", prog};
-char const *b[]={prog, prog};
-void e(){
-	if (fork() == 0) execve(a[0],(char*const*)a,NULL);
-	else
-		if (fork() == 0) execve(b[0], (char*const*)b, NULL);
-}
-void f(){n[6]=48+i;prog[6]=48+i;/*char *s=""*/;int fd=open(n,O_CREAT|O_RDWR,S_IRUSR|S_IRGRP|S_IROTH);dprintf(fd,"int main(){return (0);}");e();close(fd);}
+char prog[]="./Sully_X";
+char *const test[]={"ls", "-l", NULL};
+char compile[50] = "gcc -Werror -Wextra -Wall -o ";
+void e(){strcat(compile, prog + 2);strcat(compile, " ");strcat(compile, n);system(compile);system(prog);};
+void f(){n[6]=48+i;prog[8]=48+i;/*char *s=""*/;int fd=open(n,O_CREAT|O_RDWR,S_IRUSR|S_IRGRP|S_IROTH);dprintf(fd,"#include<stdio.h>\nint main(){printf(\"Hello World\");}");e();close(fd);}
 int main(){if(i)f();}
