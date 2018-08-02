0f1a244fc7de102b6f1557019620f913a8d6282c
bhamidi <bhamidi@student.42.fr>
2018-08-02 19:15:05 +0200

    Grace may OK

--- Go/Grace/Grace.go
+++ Go/Grace/Grace.go
@@ -1,6 +1,7 @@
 package main
 import (
 	"os"
+	"fmt"
 )
 /*
 	Rust
@@ -8,4 +9,4 @@ import (
 const n="Grace_kid.go"
 const p=os.O_RDWR|os.O_CREATE;
 const m os.FileMode=0755;
-func main(){f,_ := os.OpenFile(n, p, m);f.WriteString("fleur de la cours");f.Close();}
+func main(){s:="package main%cimport (%c%c%cos%c%c%c%cfmt%c%c)%c/*%c%cRust%c*/%cconst n=%cGrace_kid.go%c%cconst p=os.O_RDWR|os.O_CREATE;%cconst m os.FileMode=0755;%cfunc main(){s:=%c%s%c;r:=fmt.Sprintf(s,10,10,9,34,34,10,9,34,34,10,10,10,9,10,10,34,34,10,10,10,34,s,34,10);f,_ := os.OpenFile(n, p, m);f.WriteString(r);f.Close();}%c";r:=fmt.Sprintf(s,10,10,9,34,34,10,9,34,34,10,10,10,9,10,10,34,34,10,10,10,34,s,34,10);f,_ := os.OpenFile(n, p, m);f.WriteString(r);f.Close();}
