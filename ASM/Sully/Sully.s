a20a704f550d31915955a29fdbc6dedc5688bf9a
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-14 12:35:07 +0200

    wip

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,27 +1,17 @@
+%define F 0x0002 | 0x0200 | 0x0400
 global _main
 extern _puts
 extern _sprintf
+extern _system
 section .text
 
-putscompile:
-push rbp
-lea rdi,[rel bufcompile]
-call _puts
-pop rbp
-ret
-
-putsbuffer:
-push rbp
-lea rdi,[rel buffilename]
-call _puts
-pop rbp
-ret
-
 _main:
+cmp DWORD [rel i],0
+je end
 lea rdi,[rel buffilename]
 lea rsi,[rel filename]
 lea rdx,[rel i]
-mov rdx,[rdx]
+mov rdx, [rdx]
 push rdx
 call _sprintf
 pop rdx
@@ -31,9 +21,12 @@ mov rcx, rdx
 mov r8, rdx
 push rdx
 call _sprintf
-call putsbuffer
-call putscompile
-pop rbp
+pop r10
+mov rax, 0x02000000 | 5
+lea rdi, [rel buffilename]
+mov rsi,F
+mov rdx,654q
+syscall
 end:
 ret
 
@@ -43,5 +36,5 @@ bufcompile: resb 256
 section .data
 format: db '%d',10,0
 i: dd 5
-filename: db 'Sully_%d.c',0
-compile: db 'nasm -fmacho64 Sully_%d.c && gcc Sully_%d.o && ./Sully_%d',0
+filename: db 'Sully_%d.s',0
+compile: db 'nasm -fmacho64 Sully_%d.s && gcc Sully_%d.o && ./Sully_%d',0
