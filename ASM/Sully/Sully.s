1e575c738f5af4ca8294fd6356f4df884c9b7ec2
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-14 12:21:27 +0200

    wip

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,23 +1,47 @@
-%define F 0x0002 | 0x0200 | 0x0400
 global _main
 extern _puts
+extern _sprintf
 section .text
-_main:
-mov r10, value
-cmp r10,0
-je end
 
-lea rdi,[rel filename]
-mov [rdi+6], r10
+putscompile:
 push rbp
+lea rdi,[rel bufcompile]
 call _puts
 pop rbp
 ret
 
+putsbuffer:
+push rbp
+lea rdi,[rel buffilename]
+call _puts
+pop rbp
+ret
+
+_main:
+lea rdi,[rel buffilename]
+lea rsi,[rel filename]
+lea rdx,[rel i]
+mov rdx,[rdx]
+push rdx
+call _sprintf
+pop rdx
+lea rdi,[rel bufcompile]
+lea rsi,[rel compile]
+mov rcx, rdx
+mov r8, rdx
+push rdx
+call _sprintf
+call putsbuffer
+call putscompile
+pop rbp
 end:
 ret
 
+section .bss
+buffilename: resb 256
+bufcompile: resb 256
 section .data
-value: db 0x05
-filename: db "Sully_X.c",0
-prog: db "./Sully_X",0
+format: db '%d',10,0
+i: dd 5
+filename: db 'Sully_%d.c',0
+compile: db 'nasm -fmacho64 Sully_%d.c && gcc Sully_%d.o && ./Sully_%d',0
