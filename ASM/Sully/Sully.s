1220f11d6b3025094880b78013495f7963517405
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-14 13:16:24 +0200

    wip

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -1,7 +1,7 @@
 %define F 0x0002 | 0x0200 | 0x0400
 global _main
-extern _puts
 extern _sprintf
+extern _dprintf
 extern _system
 section .text
 
@@ -19,6 +19,7 @@ lea rdi,[rel bufcompile]
 lea rsi,[rel compile]
 mov rcx, rdx
 mov r8, rdx
+mov r9, rdx
 push rdx
 call _sprintf
 pop r10
@@ -26,7 +27,7 @@ mov rax, 0x02000000 | 5
 lea rdi, [rel buffilename]
 mov rsi,F
 mov rdx,654q
-syscall
+syscall ; i in r10
 end:
 ret
 
@@ -37,4 +38,6 @@ section .data
 format: db '%d',10,0
 i: dd 5
 filename: db 'Sully_%d.s',0
-compile: db 'nasm -fmacho64 Sully_%d.s && gcc Sully_%d.o && ./Sully_%d',0
+compile: db 'nasm -fmacho64 Sully_%d.s && gcc Sully_%d.o -o Sully_%d && ./Sully_%d',0
+data: db 'global _main%c_main:%cpush rbp%cpush 10%cpop rbp%cpop rbp%cret%c%c',0
+test: db 'testing',0
