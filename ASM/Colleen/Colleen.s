86d4e5d2e3d3d9093756e30f57409bfabc5921c4
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-21 15:31:06 +0200

    update comment form

--- ASM/Colleen/Colleen.s
+++ ASM/Colleen/Colleen.s
@@ -1,14 +1,20 @@
 global _main
 extern _printf
 section .text
-;dart
+;
+; dart
+;
 ft:
 ret
 _main:
 push rbp
 call ft
 pop rbp
-lea rdi,[rel s];print parameter
+lea rdi,[rel s]
+
+;
+; print parameter
+;
 mov rsi,10
 mov rdx,10
 mov rcx,10
@@ -19,17 +25,17 @@ push 34
 lea r13,[rel s]
 push r13
 push 34
-mov r15,29
+mov r15,35
 pus: push 10
 dec r15
 cmp r15,0
 jne pus
 call _printf
-mov r15,33
+mov r15,39
 po: pop rax
 dec r15
 cmp r15,0
 jne po
 ret
 section .data
-s: db "global _main%cextern _printf%csection .text%c;dart%cft:%cret%c_main:%cpush rbp%ccall ft%cpop rbp%clea rdi,[rel s];print parameter%cmov rsi,10%cmov rdx,10%cmov rcx,10%cmov r8,10%cmov r9,10%cpush 10%cpush 34%clea r13,[rel s]%cpush r13%cpush 34%cmov r15,29%cpus: push 10%cdec r15%ccmp r15,0%cjne pus%ccall _printf%cmov r15,33%cpo: pop rax%cdec r15%ccmp r15,0%cjne po%cret%csection .data%cs: db %c%s%c,0%c",0
+s: db "global _main%cextern _printf%csection .text%c;%c; dart%c;%cft:%cret%c_main:%cpush rbp%ccall ft%cpop rbp%clea rdi,[rel s]%c%c;%c; print parameter%c;%cmov rsi,10%cmov rdx,10%cmov rcx,10%cmov r8,10%cmov r9,10%cpush 10%cpush 34%clea r13,[rel s]%cpush r13%cpush 34%cmov r15,35%cpus: push 10%cdec r15%ccmp r15,0%cjne pus%ccall _printf%cmov r15,39%cpo: pop rax%cdec r15%ccmp r15,0%cjne po%cret%csection .data%cs: db %c%s%c,0%c",0
--- Go/Colleen/Colleen.go
+++ Go/Colleen/Colleen.go
@@ -4,4 +4,8 @@ import "fmt"
 	outside
 */
 func sum(a int, b int) int {return a + b;}
-func main(){/*inside*/sum(21,21);const s="package main%cimport %cfmt%c%c%c%c%c%coutside%c%c%c%cfunc sum(a int, b int) int {return a + b;}%cfunc main(){/*inside*/sum(21,21);const s=%c%s%c;fmt.Printf(s,10,34,34,10,47,42,10,9,10,42,47,10,10,34,s,34,10);}%c";fmt.Printf(s,10,34,34,10,47,42,10,9,10,42,47,10,10,34,s,34,10);}
+func main(){
+/*
+  inside
+*/
+sum(21,21);const s="package main%cimport %cfmt%c%c%c%c%c%coutside%c%c%c%cfunc sum(a int, b int) int {return a + b;}%cfunc main(){%c/*%c  inside%c*/%csum(21,21);const s=%c%s%c;fmt.Printf(s,10,34,34,10,47,42,10,9,10,42,47,10,10,10,10,10,10,34,s,34,10);}%c";fmt.Printf(s,10,34,34,10,47,42,10,9,10,42,47,10,10,10,10,10,10,34,s,34,10);}
