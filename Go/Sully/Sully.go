9b3644ae2ec02ce558d2faec2b65592190550d40
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-22 18:17:11 +0200

    wip

--- Go/Sully/Sully.go
+++ Go/Sully/Sully.go
@@ -9,4 +9,4 @@ var s ="package main%cimport (%c%cfmt%c%c%cos%c%c%cos/exec%c%c)%cvar i=%d;%cvar 
 func compile(name,output string){var cmd=exec.Command("go","build","-o",output,name);cmd.Run();}
 func run(bin string){exec.Command(bin).Run();}
 func write(filename string){var file,_ = os.OpenFile(filename,os.O_RDWR|os.O_CREATE,0755);file.WriteString(fmt.Sprintf(s,10,10,34,34,10,34,34,10,34,34,10,10,i,10,34,s,34,10,34,34,34,34,34,34,10,10,10,34,34,34,34,34,34,34,34,34,34,34,34,34,34,10));file.Close();};
-func main(){if(i!=0){write(fmt.Sprintf("Sully_%d.go",i));compile(fmt.Sprintf("Sully_%d.go",i),fmt.Sprintf("Sully_%d",i));i--;write(fmt.Sprintf("Sully_%d.go",i));compile(fmt.Sprintf("Sully_%d.go",i),fmt.Sprintf("Sully_%d",i));run(fmt.Sprintf("./Sully_%d",i));}}
+func main(){if(i>0){write(fmt.Sprintf("Sully_%d.go",i));compile(fmt.Sprintf("Sully_%d.go",i),fmt.Sprintf("Sully_%d",i));i--;write(fmt.Sprintf("Sully_%d.go",i));compile(fmt.Sprintf("Sully_%d.go",i),fmt.Sprintf("Sully_%d",i));run(fmt.Sprintf("./Sully_%d",i));}}
