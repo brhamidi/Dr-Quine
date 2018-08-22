b68ed208f70e4291d460c4218cf3618b373cbe85
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-22 17:58:39 +0200

    ok Go

--- Go/Sully/Sully.go
+++ Go/Sully/Sully.go
@@ -1,14 +1,12 @@
 package main
 import (
-	"fmt"
-	"os"
-	"os/exec"
+"fmt"
+"os"
+"os/exec"
 )
-const i=5;
-var filename="Sully";
-var n=fmt.Sprintf("%s_%c.go", filename,48+i);
-var bin=fmt.Sprintf("./Sully_%c", 48+i);
-var file,_ = os.OpenFile(n,os.O_RDWR|os.O_CREATE,0755);
-func e(){cmd := exec.Command("go", "build", "-o", fmt.Sprintf("%s_%c",filename,48+i),n);cmd.Run();cmd=exec.Command(bin);cmd.Run();}
-func f(){s:="package main%cimport (%c%c%cfmt%c%c%c%cos%c%c%c%cos/exec%c%c)%cconst i=%c;%cvar filename=%cSully%c;%cvar n=fmt.Sprintf(%c%%s_%%c.go%c, filename,48+i);%cvar bin=fmt.Sprintf(%c./Sully_%%c%c, 48+i);%cvar file,_ = os.OpenFile(n,os.O_RDWR|os.O_CREATE,0755);%cfunc e(){cmd := exec.Command(%cgo%c, %cbuild%c, %c-o%c, fmt.Sprintf(%c%%s_%%c%c,filename,48+i),n);cmd.Run();cmd=exec.Command(bin);cmd.Run();}%cfunc f(){s:=%c%s%c;file.WriteString(fmt.Sprintf(s,10,10,9,34,34,10,9,34,34,10,9,34,34,10,10,48+i-1,10,34,34,10,34,34,10,34,34,10,10,34,34,34,34,34,34,34,34,10,34,s,34,10,10));file.Close();};%cfunc main(){if(i!=0){f();e();}}%c";file.WriteString(fmt.Sprintf(s,10,10,9,34,34,10,9,34,34,10,9,34,34,10,10,48+i-1,10,34,34,10,34,34,10,34,34,10,10,34,34,34,34,34,34,34,34,10,34,s,34,10,10));file.Close();};
-func main(){if(i!=0){f();e();}}
+var i=5;
+var s ="package main%cimport (%c%cfmt%c%c%cos%c%c%cos/exec%c%c)%cvar i=%d;%cvar s =%c%s%c;%cfunc compile(name,output string){var cmd=exec.Command(%cgo%c,%cbuild%c,%c-o%c,output,name);cmd.Run();}%cfunc run(bin string){exec.Command(bin).Run();}%cfunc write(filename string){var file,_ = os.OpenFile(filename,os.O_RDWR|os.O_CREATE,0755);file.WriteString(fmt.Sprintf(s,10,10,34,34,10,34,34,10,34,34,10,10,i,10,34,s,34,10,34,34,34,34,34,34,10,10,10,34,34,34,34,34,34,34,34,34,34,34,34,34,34,10));file.Close();};%cfunc main(){if(i!=0){write(fmt.Sprintf(%cSully_%%d.go%c,i));compile(fmt.Sprintf(%cSully_%%d.go%c,i),fmt.Sprintf(%cSully_%%d%c,i));i--;write(fmt.Sprintf(%cSully_%%d.go%c,i));compile(fmt.Sprintf(%cSully_%%d.go%c,i),fmt.Sprintf(%cSully_%%d%c,i));run(fmt.Sprintf(%c./Sully_%%d%c,i));}}%c";
+func compile(name,output string){var cmd=exec.Command("go","build","-o",output,name);cmd.Run();}
+func run(bin string){exec.Command(bin).Run();}
+func write(filename string){var file,_ = os.OpenFile(filename,os.O_RDWR|os.O_CREATE,0755);file.WriteString(fmt.Sprintf(s,10,10,34,34,10,34,34,10,34,34,10,10,i,10,34,s,34,10,34,34,34,34,34,34,10,10,10,34,34,34,34,34,34,34,34,34,34,34,34,34,34,10));file.Close();};
+func main(){if(i!=0){write(fmt.Sprintf("Sully_%d.go",i));compile(fmt.Sprintf("Sully_%d.go",i),fmt.Sprintf("Sully_%d",i));i--;write(fmt.Sprintf("Sully_%d.go",i));compile(fmt.Sprintf("Sully_%d.go",i),fmt.Sprintf("Sully_%d",i));run(fmt.Sprintf("./Sully_%d",i));}}
