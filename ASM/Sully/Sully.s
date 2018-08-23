e31b1163e5d07191367bf0789258097ea5864498
Brahim Hamidi <bhamidi@student.42.fr>
2018-08-23 17:30:12 +0200

    wip :)

--- ASM/Sully/Sully.s
+++ ASM/Sully/Sully.s
@@ -18,6 +18,8 @@ lea rdi,[rel buf]
 mov rsi,0x0002 | 0x0200 | 0x0400
 mov rdx,654q
 syscall
+pop r13
+push r13
 push rbp
 push rax
 mov rdi,rax
@@ -26,18 +28,34 @@ mov rdx,10
 mov rcx,10
 mov r8,10
 mov r9,10
-mov r15,2
+push 10
+push r13
+push 10
+push 39
+lea r13,[rel data]
+push r13
+push 39
+push 10
+push 39
+push 39
+push 10
+push 39
+push 39
+push 10
+push 39
+push 39
+mov r15,1
 pus: push 10
 dec r15
 cmp r15,0
 jne pus
 call _dprintf
-mov r15,2
+mov r15,16
 po: pop rax
 dec r15
 cmp r15,0
 jne po
-pop rdi ; put fd for close()
+pop rdi
 pop rbp
 mov rax,0x02000000 | 6
 syscall
@@ -79,5 +97,5 @@ section .data
 compile: db 'nasm -fmacho64 Sully_%d.s && gcc Sully_%d.o -o Sully_%d && rm -f Sully_%d.o',0
 filename: db 'Sully_%d.s',0
 run: db './Sully_%d',0
-data: db '; first comment%c; second comment%cglobal _main%csection .text%c_main:%cret%c',0
+data: db 'global _main%cextern _sprintf%cextern _dprintf%cextern _printf%csection .data%ccompile: db %cnasm -fmacho64 Sully_%%d.s && gcc Sully_%%d.o -o Sully_%%d && rm -f Sully_%%d.o%c,0%cfilename: db %cSully_%%d.s%c,0%crun: db %c./Sully_%%d%c,0%cdata: db %c%s%c,0%ci: dd %d%c',0
 i: dd 5
